2a56031421dbe35e5194e448ccbc8111ddff10b1
sanoto <kmn3rei@gmail.com>
2020-07-06 13:03:11 +0900

    [fix] titleとthemeの設定が間違ってたので修正

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,16 @@ class EnhanTodo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
+      title: 'EnhanTodo',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.green,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blueGrey[800],
+        accentColor: Colors.green,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('EnhanTodo'),
