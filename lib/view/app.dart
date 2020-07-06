f6288d71fbb7671efa82d7301048a6a6a90148bd
sanoto <kmn3rei@gmail.com>
2020-07-06 20:51:16 +0900

    [add] TodoListを追加してviewmodelを使えるようにした

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -1,67 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:enhantodo/viewmodel/app_viewmodel.dart';
+import 'package:enhantodo/view/todo_list.dart';
 
 class EnhanTodo extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-    title: 'EnhanTodo',
-    theme: ThemeData(
-      brightness: Brightness.light,
-      primarySwatch: Colors.green,
-    ),
-    darkTheme: ThemeData(
-      brightness: Brightness.dark,
-      primaryColor: Colors.blueGrey[800],
-      accentColor: Colors.green,
-    ),
-    home: Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          const SliverAppBar(
-            flexibleSpace: const FlexibleSpaceBar(
-              title: Text('EnhanTodo'),
+        title: 'EnhanTodo',
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.green,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.blueGrey[800],
+          accentColor: Colors.green,
+        ),
+        home: ChangeNotifierProvider(
+          create: (_) => EnhanTodoViewModel(TodoList()),
+          child: Scaffold(
+            body: CustomScrollView(
+              slivers: <Widget>[
+                const SliverAppBar(
+                  flexibleSpace: const FlexibleSpaceBar(
+                    title: Text('EnhanTodo'),
+                  ),
+                  stretch: false,
+                  floating: true,
+                  pinned: false,
+                  snap: false,
+                ),
+                Consumer(
+                  builder: (_, EnhanTodoViewModel viewModel, __) =>
+                      viewModel.sliver,
+                ),
+              ],
             ),
-            stretch: false,
-            floating: true,
-            pinned: false,
-            snap: false,
-          )
-        ],
-      ),
-      drawer: _DrawerWidget(),
-    ),
-  );
+            drawer: _DrawerWidget(),
+          ),
+        ),
+      );
 }
 
 class _DrawerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Drawer(
-    child: ListView(
-      padding: EdgeInsets.zero,
-      children: <Widget>[
-        DrawerHeader(
-          child: Text(
-            'EnhanTodo',
-            style: Theme.of(context).primaryTextTheme.headline5,
-          ),
-          decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
-          ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'EnhanTodo',
+                style: Theme.of(context).primaryTextTheme.headline5,
+              ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.event_note),
+              title: Text('Todo List'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
-        ListTile(
-          leading: Icon(Icons.event_note),
-          title: Text('Todo List'),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.settings),
-          title: Text('Settings'),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-      ],
-    ),
-  );
+      );
 }
---
+++ lib/view/todo_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (BuildContext context, int index) {
+            final itemIndex = index ~/ 2;
+            if (index.isEven)
+              return ListTile(
+                title: Text('task$itemIndex'),
+              );
+            else
+              return Divider(
+                height: 0,
+                thickness: 1,
+              );
+          },
+          semanticIndexCallback: (_, int localIndex) {
+            if (localIndex.isEven) return localIndex ~/ 2;
+            return null;
+          },
+        ),
+      );
+}
